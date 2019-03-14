f3dab48bfa5b1f4c9487a44ec2b00ab2ab27d5e1
pasindusiriwardana <pasindulasithjk@gmail.com>
2019-03-15 00:27:11 +0530

    initialising

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
 
+export interface Todo{
+  task:string;
+  priority:number;
+  createdAt:number;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  private todoCollection: AngularFirestoreCollection<Todo>;
+  private todos: Observable<Todo[]>;
+
+  constructor(db: AngularFirestore) {
+    this.todoCollection = db.collection<Todo>('todos');
+
+    // this.todos = this.todoCollection.snapshotChanges().pipe(
+      
+    // )
+   }
 }
